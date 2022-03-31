0ded98f7ce10f2c34b909d4fb1e1c32bd8c22959
trastephenson <mrtravisstephenson@gmail.com>
2022-03-30 17:51:42 -0700

    Add files via upload

---
+++ lib/Help.ex
@@ -0,0 +1,18 @@
+  defmodule Help do
+   use Hound.Helpers
+
+  
+  
+
+##  def find_img do
+  #   elements = find_all_elements(:tag, "img") 
+  #    IO.inspect(elements, label: :element)
+   #   Enum.each(elements,fn x -> 
+    #    src = attribute_value(x,"src")
+     #   IO.inspect(src)
+      #  result = navigate_to(src)
+       # IO.inspect(result)
+      
+  # end)
+  # end
+end  
